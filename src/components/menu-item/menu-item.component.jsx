ea7030e4cf36100ae51f17274b00e88657a4770b
himath <himath.m.perera@gmail.com>
2020-07-18 01:12:24 +0530

    hompage Ui and broke into components

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./menu-item.style.scss";
+
+export default function MenuItem({ title, imageUrl, size }) {
+  return (
+    <div className={`menu-item ${size}`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">shop Now</span>
+      </div>
+    </div>
+  );
+}
