0567f4532a62d30574aa20cf5312efd93a3d9108
himath <himath.m.perera@gmail.com>
2020-11-27 10:34:10 +0530

    made shop data an object instead of an array to optimize app

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import Collection from "../../components/collection-preview/collection-preview.component";
 
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
-import {selectShopItems} from "../../redux/shop/shop.selector"
-import {createStructuredSelector} from "reselect"
+import { selectShopItemsForOverview } from "../../redux/shop/shop.selector";
+import { createStructuredSelector } from "reselect";
 
-import "./collection-overview.styles.scss"
+import "./collection-overview.styles.scss";
 
-const CollectionOverview = ({collection_data}) =>(
-    <div className="collections-overview">
-     {collection_data.map((items) => (
-        <Collection
-          key={items.id}
-          title={items.title}
-          items={items.items}
-          routeName={items.routeName}
-        />
-      ))}
-    </div>
-)
-
+const CollectionOverview = ({ collection_data }) => (
+  <div className="collections-overview">
+    {collection_data.map((items) => (
+      <Collection
+        key={items.id}
+        title={items.title}
+        items={items.items}
+        routeName={items.routeName}
+      />
+    ))}
+  </div>
+);
 
 const mapStateToProps = createStructuredSelector({
-    collection_data : selectShopItems
-  })
+  collection_data: selectShopItemsForOverview,
+});
 
-export default connect(mapStateToProps)( CollectionOverview)
+export default connect(mapStateToProps)(CollectionOverview);
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,22 +1,16 @@
-import {createSelector} from "reselect"
+import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-    hats : 1,
-    sneakers :2,
-    jackets:3,
-    womens:4,
-    mens:5
-} 
-
-const selectShop =state =>state.shop
+const selectShop = (state) => state.shop;
 
 export const selectShopItems = createSelector(
-    [selectShop],
-    shop =>shop.shopData
-)
+  [selectShop],
+  (shop) => shop.shopData
+);
 
-export const selectCollection = collectionUrlParam => 
-createSelector (
-    [selectShopItems],
-    shopData => shopData.find(shopData => shopData.id === COLLECTION_ID_MAP[collectionUrlParam])
-)
+export const selectShopItemsForOverview = createSelector(
+  [selectShopItems],
+  (shopData) => Object.keys(shopData).map((key) => shopData[key])
+);
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector([selectShopItems], (shopData) => shopData[collectionUrlParam]);
